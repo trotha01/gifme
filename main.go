d1de7746e79303f9f3dbb237709375b26b4a4589
Trevor Rothaus <trevor.rothaus@sendgrid.com>
2015-06-10 04:40:29 -0700

    prints inline image

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
 )
 
 type giphyImage struct {
@@ -28,12 +34,25 @@ const searchURL = "http://api.giphy.com/v1/gifs/search"
 const apiKey = "dc6zaTOxFJmzC"
 
 func main() {
-	searchTerm := "doggie"
-	reqURL := fmt.Sprintf("%s?q=%s&api_key=%s", searchURL, searchTerm, apiKey)
-	log.Printf("req: %s", reqURL)
-	resp, err := http.Get(reqURL)
+	if len(os.Args) < 1 {
+		log.Fatal("Please enter a gif to search for")
+	}
+	searchTerm := strings.Join(os.Args[1:], " ")
+
+	log.Printf("Powered by Giphy")
+	log.Printf("Searching for %s...", searchTerm)
+
+	u, err := url.Parse(searchURL)
 	if err != nil {
-		log.Fatalf("Error getting gif: %s", err.Error())
+		log.Fatalf("url parse error: %s", err.Error())
+	}
+	q := u.Query()
+	q.Set("api_key", apiKey)
+	q.Add("q", searchTerm)
+	u.RawQuery = q.Encode()
+	resp, err := http.Get(u.String())
+	if err != nil {
+		log.Fatalf("Error getting gifs: %s", err.Error())
 	}
 	defer resp.Body.Close()
 
@@ -43,8 +62,40 @@ func main() {
 		log.Fatalf("response: %s", body)
 	}
 
+	// unmarshal response
 	gifResp := giphy{}
 	json.Unmarshal(body, &gifResp)
-	log.Printf("Gif: %s", body)
-	log.Printf("Gif: %+v", gifResp)
+
+	// randomly choose a gif from the response
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	gifCount := len(gifResp.Data)
+	gifChoice := gifResp.Data[r.Intn(gifCount)]
+
+	// get original gif
+	resp, err = http.Get(gifChoice.Images.Original.URL)
+	if err != nil {
+		log.Fatalf("Error getting original gif: %s", err.Error())
+	}
+	defer resp.Body.Close()
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if resp.StatusCode != 200 {
+		log.Printf("response code: %s", resp.Status)
+		log.Fatalf("response: %s", body)
+	}
+
+	// create tmp gif file
+	file, err := os.Create("tmpGif")
+	if err != nil {
+		log.Fatalf("Error creating tmp file: %s", err.Error())
+	}
+	_, err = file.Write(body)
+	if err != nil {
+		log.Fatalf("Error writting gif to tmp file: %s", err.Error())
+	}
+
+	// print inline image
+	b64FileName := base64.StdEncoding.EncodeToString([]byte(file.Name()))
+	b64FileContents := base64.StdEncoding.EncodeToString(body)
+	fmt.Printf("\033]1337;File=name=%s;inline=1:%s\a\n", b64FileName, b64FileContents)
 }
